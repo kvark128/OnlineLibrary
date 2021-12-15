8fba9b9e7cde15f652bf4347ac8a17623b74aff4
Kvark <kvark128@yandex.ru>
2021-12-15 16:50:55 +0800

    Helper structures for synchronized IO moved to util/syncio package

--- internal/player/fragment.go
+++ internal/player/fragment.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/kvark128/OnlineLibrary/internal/gui"
 	"github.com/kvark128/OnlineLibrary/internal/util"
+	"github.com/kvark128/OnlineLibrary/internal/util/syncio"
 	"github.com/kvark128/OnlineLibrary/internal/waveout"
 	"github.com/kvark128/minimp3"
 	"github.com/kvark128/sonic"
@@ -62,8 +63,8 @@ func NewFragment(mp3 io.Reader, devName string) (*Fragment, error) {
 func (f *Fragment) play(playing *util.Flag) error {
 	var p int64
 	wp := bufio.NewWriterSize(f.wp, buffer_size)
-	stream := util.NewReadWriter(f.stream, f)
-	dec := util.NewReadSeeker(f.dec, f)
+	stream := syncio.NewReadWriter(f.stream, f)
+	dec := syncio.NewReader(f.dec, f)
 	defer f.wp.Close()
 
 	for playing.IsSet() {
--- internal/util/syncio/syncio.go
+++ internal/util/syncio/syncio.go
@@ -1,4 +1,4 @@
-package util
+package syncio
 
 import (
 	"io"
