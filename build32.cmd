ec335c1ee252af5bcbc306ac59bcb127c1f51cf4
Kvark <kvark128@yandex.ru>
2022-02-12 07:08:34 +0800

    Linker now uses the -s option

--- build32.cmd
+++ build32.cmd
@@ -3,5 +3,5 @@ rsrc -arch 386 -manifest OnlineLibrary.exe.manifest -o rsrc.syso
 set GOOS=windows
 set GOARCH=386
 set CGO_ENABLED=1
-go build -tags walk_use_cgo -ldflags "-H=windowsgui"
+go build -tags walk_use_cgo -ldflags "-s -H=windowsgui"
 del rsrc.syso
--- build64.cmd
+++ build64.cmd
@@ -3,5 +3,5 @@ rsrc -arch amd64 -manifest OnlineLibrary.exe.manifest -o rsrc.syso
 set GOOS=windows
 set GOARCH=amd64
 set CGO_ENABLED=1
-go build -tags walk_use_cgo -ldflags "-H=windowsgui"
+go build -tags walk_use_cgo -ldflags "-s -H=windowsgui"
 del rsrc.syso
