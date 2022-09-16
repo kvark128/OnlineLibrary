df39f823c5da062e188d5d51c1e4cc90131bf73b
Kvark <kvark128@yandex.ru>
2022-09-17 02:42:42 +0800

    Some fixes

--- build32.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-go run cmd/rsrc/rsrc.go -arch 386 -manifest OnlineLibrary.exe.manifest -o rsrc.syso
-if errorlevel 1 exit /b %errorlevel%
-set GOOS=windows
-set GOARCH=386
-set CGO_ENABLED=1
-go build -tags walk_use_cgo -ldflags "-s -H=windowsgui"
-del rsrc.syso
