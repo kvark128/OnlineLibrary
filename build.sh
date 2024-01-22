35e4eed2689f35b6534630027c15899c3e731bad
Kvark <kvark128@yandex.ru>
2024-01-22 08:18:12 +0800

    Improvements in build.sh

--- build.sh
+++ build.sh
@@ -24,17 +24,15 @@ EXTERNAL_DIR="$PWD/external"
 SONIC_DIR="$EXTERNAL_DIR/sonic"
 MINIMP3_DIR="$EXTERNAL_DIR/minimp3"
 
-$CC $CFLAGS -c -o $SONIC_DIR/sonic.o $SONIC_DIR/sonic.c
-ar rcs $SONIC_DIR/libsonic.a $SONIC_DIR/sonic.o
-install -D -p $SONIC_DIR/sonic.h $INCLUDE_DIR/sonic.h
-install -D -p $SONIC_DIR/libsonic.a $LIB_DIR/libsonic.a
-install -D -p $MINIMP3_DIR/minimp3.h $INCLUDE_DIR/minimp3.h
-rm $SONIC_DIR/*.o $SONIC_DIR/*.a
+mkdir -p $BUILD_DIR
+mkdir -p $INCLUDE_DIR
+mkdir -p $LIB_DIR
 
-export CGO_ENABLED=1
-export CGO_CFLAGS="-I$INCLUDE_DIR $CFLAGS"
-export CGO_LDFLAGS=-L$LIB_DIR
-export GOOS=windows
+$CC $CFLAGS -c -o $BUILD_DIR/sonic.o $SONIC_DIR/sonic.c
+ar rcs $LIB_DIR/libsonic.a $BUILD_DIR/sonic.o
+install -D -p $SONIC_DIR/sonic.h $INCLUDE_DIR/sonic.h
+install -D -p $MINIMP3_DIR/minimp3.h $INCLUDE_DIR/minimp3.h
 
 go run cmd/rsrc/rsrc.go -arch $ARCH -manifest OnlineLibrary.exe.manifest -o "rsrc_windows_$ARCH.syso"
-go build -tags walk_use_cgo -ldflags "-s -H=windowsgui"
+env GOOS=windows CGO_ENABLED=1 CGO_CFLAGS="-I$INCLUDE_DIR $CFLAGS" CGO_LDFLAGS=-L$LIB_DIR \
+	go build -tags walk_use_cgo -ldflags "-s -H=windowsgui"
